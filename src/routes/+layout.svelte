eee4c137cf4660ba8673cb503de9afe102ffdcb9
Anna Pollard <annapollard@Annas-Air.lan>
2023-06-08 14:25:15 -0400

    first

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,29 @@
+<script>
+    import '$lib/app.css'
+    import Header from "$lib/Header.svelte";
+    import Footer from "$lib/Footer.svelte";
+
+</script>
+<div class="wrapper">
+<Header logo="My Fun Science"/>
+<main>
+<slot />
+</main>
+<Footer />
+</div>
+
+<style>
+    .wrapper {
+        min-height: 100dvh;
+        display: grid;
+        grid-template-rows: auto 1fr auto;
+    }
+    main {
+        width:  100%;
+        max-width: 64em;
+        padding-block: 6em; 
+        padding-inline: 3em;
+        margin-inline: auto;
+        
+    }
+</style>
---
+++ src/routes/gallery/+page.js
@@ -0,0 +1,11 @@
+// The SvelteKit "Load function" using a custom fetch() that works on the server
+// as well as the client.
+export async function load({ fetch }) {
+  const endpoint =
+    'https://public-api.wordpress.com/rest/v1.1/sites/repo995752852.wordpress.com/posts/?category=art-class'
+
+  const response = await fetch(endpoint)
+  const posts = await response.json()
+
+  return { posts }
+}
